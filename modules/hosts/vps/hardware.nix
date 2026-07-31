a5322fe50b1fcc4f42e8996d4839e1f8be3a240d
Gabriel Ribeiro <ic.gabrielribeiro@gmail.com>
2026-07-31 07:56:17 -0300

    fix: include OCI VirtIO SCSI boot drivers

--- modules/hosts/vps/hardware.nix
+++ modules/hosts/vps/hardware.nix
@@ -4,7 +4,9 @@
     boot.initrd.availableKernelModules = [
       "virtio_pci"
       "virtio_blk"
+      "virtio_scsi"
       "virtio_net"
+      "sd_mod"
     ];
 
     nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
